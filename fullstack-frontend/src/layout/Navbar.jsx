6e65d48226384f338dc6acdd4d5e7635a912673c
Diogo Sarti <diogosarti13@gmail.com>
2023-03-28 16:10:16 -0300

    Add user button finished

--- fullstack-frontend/src/layout/Navbar.jsx
+++ fullstack-frontend/src/layout/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import AddUser from '../users/AddUser'
 
 export default function Navbar() {
     return (
         <div>
             <nav data-bs-theme="dark" className="navbar navbar-expand-md bg-primary">
                 <div className="container">
-                    <a className="navbar-brand" href="#">Full Stack Application</a>
+                    <a className="navbar-brand" href="/">Full Stack Application</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -14,12 +15,17 @@ export default function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <button className='btn btn-outline-light'>Add User</button>
+                                <button type="button" className='btn btn-outline-light' data-bs-toggle="modal" data-bs-target="#addUserModal">Add User</button>
+
+
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
+            <div className="modal fade" id="addUserModal" tabIndex="-1" aria-labelledby="addUserModalLabel" aria-hidden="true">
+                <AddUser />
+            </div>
 
         </div>
     )
--- fullstack-frontend/src/users/AddUser.jsx
+++ fullstack-frontend/src/users/AddUser.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 export default function AddUser() {
-  return (
-    <div>AddUser</div>
-  )
+
+    const BASE_URL = "http://localhost:8080"
+
+    const [user, setUser] = useState({
+        name: "",
+        username: "",
+        email: ""
+    })
+
+    const { name, username, email } = user
+
+    const onInputChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await axios.post(BASE_URL + "/user", user)
+            setUser({
+                name: "",
+                username: "",
+                email: ""
+            })
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    return (
+        <div className="modal-dialog modal-lg">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h1 className="modal-title text-center fs-5" id="addUserModalLabel">Adicionar Usuário</h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form onSubmit={(e) => onSubmit(e)}>
+                    <div className="modal-body">
+                        <div className='container-fluid'>
+                            <div className='row'>
+                                <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
+                                    <label htmlFor='name' className='formLabel mt-2'>Name</label>
+                                    <input type={"text"} className={"form-control mt-2"} value={name} onChange={(e) => onInputChange(e)} placeholder="Digite o nome" name='name' />
+                                    <label htmlFor='Username' className='formLabel mt-2'>Username</label>
+                                    <input type={"text"} className={"form-control mt-2"} value={username} onChange={(e) => onInputChange(e)} placeholder="Digite o username" name='username' />
+                                    <label htmlFor='Email' className='formLabel mt-2'>Email</label>
+                                    <input type={"text"} className={"form-control mt-2"} value={email} onChange={(e) => onInputChange(e)} placeholder="Digite o email" name='email' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" className="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+
+
+    )
 }
